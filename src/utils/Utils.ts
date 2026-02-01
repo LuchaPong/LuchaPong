0fd2e5ec08e6675299b27374ad7e2f2727ed2ff6
Uday Patel <19371077+DTBUday@users.noreply.github.com>
2026-02-01 12:57:38 +0100

    Refactor utils for creating text

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,27 +1,45 @@
 import { Scene } from "phaser";
 
-export function createInteractiveButton(context: Scene, label: string, x: number, y: number, fn: Function): Phaser.GameObjects.Sprite {
-	const btn = context.add.sprite(x, y, "button").setInteractive().setFrame(1);
-	context.add
-		.text(x, y, label, {
-			fontFamily: "Arial Black",
-			fontSize: 38,
-			color: "#ffffff",
-			stroke: "#000000",
-			strokeThickness: 8,
-			align: "center"
-		})
-		.setOrigin(0.5, 0.7);
-	btn.on("pointerover", () => {
-		btn.setFrame(0);
-	});
+export function createInteractiveButton(
+  context: Scene,
+  label: string,
+  x: number,
+  y: number,
+  fontSize: number = 38,
+  fn: Function,
+): Phaser.GameObjects.Sprite {
+  const btn = context.add.sprite(x, y, "button").setInteractive().setFrame(1);
+  createText(context, label, x, y, fontSize);
 
-	btn.on("pointerout", () => {
-		btn.setFrame(1);
-	});
+  btn.on("pointerover", () => {
+    btn.setFrame(0);
+  });
 
-	btn.on("pointerup", fn);
+  btn.on("pointerout", () => {
+    btn.setFrame(1);
+  });
 
-	return btn;
+  btn.on("pointerup", fn);
+
+  return btn;
+}
+
+export function createText(
+  context: Scene,
+  label: string,
+  x: number,
+  y: number,
+  fontSize: number,
+) {
+  context.add
+    .text(x, y, label, {
+      fontFamily: "Arial Black",
+      fontSize: fontSize,
+      color: "#ffffff",
+      stroke: "#000000",
+      strokeThickness: 8,
+      align: "center",
+    })
+    .setOrigin(0.5, 0.7);
 }
 
