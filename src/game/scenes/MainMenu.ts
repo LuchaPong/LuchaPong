0d029c5695191860a40f800b737b32e5aef1af32
Uday Patel <19371077+DTBUday@users.noreply.github.com>
2026-02-01 14:17:05 +0100

    Add shine on logo

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -5,37 +5,57 @@ import { EventBus } from "../EventBus";
 import { createInteractiveButton } from "../../utils/Utils";
 
 export class MainMenu extends Scene {
-	background: GameObjects.Image;
-	logo: GameObjects.Image;
-	startBtn: GameObjects.Sprite;
-	settingsBtn: GameObjects.Sprite;
-	quitBtn: GameObjects.Sprite;
+  background: GameObjects.Image;
+  logo: GameObjects.Image;
+  startBtn: GameObjects.Sprite;
+  settingsBtn: GameObjects.Sprite;
+  quitBtn: GameObjects.Sprite;
+  timer: Phaser.Time.TimerEvent;
 
-	constructor() {
-		super("MainMenu");
-	}
+  constructor() {
+    super("MainMenu");
+  }
 
-	create() {
-		const width = this.scale.width;
-		const height = this.scale.height;
+  create() {
+    const width = this.scale.width;
+    const height = this.scale.height;
 
-		this.background = this.add.image(width / 2, height / 2, "background");
-		this.background.setDisplaySize(width, height);
+    this.background = this.add.image(width / 2, height / 2, "background");
+    this.background.setDisplaySize(width, height);
 
-		this.logo = this.add.image(width / 2, height * 0.2, "logo").setDepth(100);
-		this.logo.setDisplaySize(width * 0.4, height * 0.4)
-		
-    this.startBtn = createInteractiveButton(this, "START", width / 2, height / 2, () => {
-			this.scene.start("Game");
-		});
-		this.settingsBtn = createInteractiveButton(this, "CONTROLS", width / 2, height / 2 + this.startBtn.frame.height, () => {
-			this.scene.start("Controls");
-		});
-		this.quitBtn = createInteractiveButton(this, "QUIT", width / 2, height / 2 + 2 * this.startBtn.frame.height, () => {
-		// this.scene.start("Quit");
-		});
+    this.logo = this.add.image(width / 2, height * 0.2, "logo").setDepth(100);
+    this.logo.setDisplaySize(width * 0.4, height * 0.4);
+    this.logo.postFX.addShine(1, 0.2, 5);
 
-		EventBus.emit("current-scene-ready", this);
-	}
-	
+    this.startBtn = createInteractiveButton(
+      this,
+      "START",
+      width / 2,
+      height / 2,
+      () => {
+        this.scene.start("Game");
+      },
+    );
+    this.settingsBtn = createInteractiveButton(
+      this,
+      "CONTROLS",
+      width / 2,
+      height / 2 + this.startBtn.frame.height,
+      () => {
+        this.scene.start("Controls");
+      },
+    );
+    this.quitBtn = createInteractiveButton(
+      this,
+      "QUIT",
+      width / 2,
+      height / 2 + 2 * this.startBtn.frame.height,
+      () => {
+        // this.scene.start("Quit");
+      },
+    );
+
+    EventBus.emit("current-scene-ready", this);
+  }
 }
+
