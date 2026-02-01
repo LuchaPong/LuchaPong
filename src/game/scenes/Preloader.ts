a1564fb1300bdc1b2cc53cb9ea555d075dc548ce
Bryan Liegsalz <bryanliegsalz@gmail.com>
2026-02-01 17:03:00 +0100

    feat: centered preloader and made it bigger

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,4 +1,5 @@
 import { Scene } from "phaser";
+import { setupBackground } from "../utils/setupBackground";
 
 export class Preloader extends Scene {
   constructor() {
@@ -6,19 +7,20 @@ export class Preloader extends Scene {
   }
 
   init() {
-    //  We loaded this image in our Boot Scene, so we can display it here
-    this.add.image(512, 384, "background");
+    setupBackground(this);
+
+    const centerX = this.scale.width / 2;
+    const centerY = this.scale.height / 2;
 
     //  A simple progress bar. This is the outline of the bar.
-    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    this.add.rectangle(centerX, centerY, 600, 48).setStrokeStyle(2, 0xffffff);
 
     //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+    const bar = this.add.rectangle(centerX - 300 + 5, centerY, 6, 40, 0xffffff);
 
     //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
     this.load.on("progress", (progress: number) => {
-      //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-      bar.width = 4 + 460 * progress;
+      bar.width = 600 * progress - 4;
     });
   }
 
@@ -78,7 +80,6 @@ export class Preloader extends Scene {
   create() {
     //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
     //  For example, you can define global animations here, so we can use them in other scenes.
-
     //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
     this.scene.start("MainMenu");
   }
